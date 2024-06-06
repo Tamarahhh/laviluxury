9dc27e0b67a194f4c414f6e6673353e1f1a6c147
Tamara Boboye <160331279+Tamarahhh@users.noreply.github.com>
2024-06-06 22:58:21 +0100

    minor change

--- src/Header.js
+++ src/Header.js
@@ -3,28 +3,50 @@ import "./Header.css";
 
 export default function Header() {
   return (
-    <div className="Header">
-      <div className="logo">
-        <h1>LaviLuxury</h1>
-        <p>Interiors</p>
+    <header className="Header navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <div className="navbar-brand logo">
+          <h1>LaviLuxury</h1>
+          <p>Interiors</p>
+        </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#about">
+                About Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#projects">
+                Projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#services">
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#contact">
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="#about">About Us</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li>
-          <a href="#services">Services</a>
-        </li>
-        <li>
-          <a href="#contact">Contact Us</a>
-        </li>
-      </ul>
-    </div>
+    </header>
   );
 }
